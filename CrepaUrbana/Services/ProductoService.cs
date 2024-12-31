291d77451ae712a98a4db5b49658bd5ecdc24681
alvinsanchez93 <159292482+alvinsanchez93@users.noreply.github.com>
2024-12-30 17:49:37 -0800

    Add files via upload

---
+++ CrepaUrbana/Services/ProductoService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using CrepaUrbana.Models;
+
+
+namespace CrepaUrbana.Services
+{
+    public class ProductoService
+    {
+        private readonly string connectionString;
+
+        public ProductoService()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["CrepaUrbanaDB"].ConnectionString;
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            var productos = new List<Producto>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM Productos";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            productos.Add(new Producto
+                            {
+                                IdProducto = (int)reader["IdProducto"],
+                                Nombre = reader["Nombre"].ToString(),
+                                Precio = (decimal)reader["Precio"],
+                                Categoria = reader["Categoria"].ToString(),
+                                Stock = (int)reader["Stock"]
+                            });
+                        }
+                    }
+                }
+            }
+            return productos;
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "INSERT INTO Productos (Nombre, Precio, Categoria, Stock) VALUES (@Nombre, @Precio, @Categoria, @Stock)";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", producto.Nombre);
+                    command.Parameters.AddWithValue("@Precio", producto.Precio);
+                    command.Parameters.AddWithValue("@Categoria", producto.Categoria);
+                    command.Parameters.AddWithValue("@Stock", producto.Stock);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ActualizarProducto(Producto producto)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE Productos SET Nombre = @Nombre, Precio = @Precio, Categoria = @Categoria, Stock = @Stock WHERE IdProducto = @IdProducto";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdProducto", producto.IdProducto);
+                    command.Parameters.AddWithValue("@Nombre", producto.Nombre);
+                    command.Parameters.AddWithValue("@Precio", producto.Precio);
+                    command.Parameters.AddWithValue("@Categoria", producto.Categoria);
+                    command.Parameters.AddWithValue("@Stock", producto.Stock);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void EliminarProducto(int idProducto)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "DELETE FROM Productos WHERE IdProducto = @IdProducto";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdProducto", idProducto);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ CrepaUrbana/ViewModels/ProductosViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CrepaUrbana.Models;
+using CrepaUrbana.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CrepaUrbana.Commands;
+
+namespace CrepaUrbana.ViewModels
+{
+    public class ProductosViewModel : INotifyPropertyChanged
+    {
+        private readonly ProductoService productoService;
+        private Producto productoSeleccionado;
+
+        public ObservableCollection<Producto> Productos { get; set; }
+
+        public Producto ProductoSeleccionado
+        {
+            get { return productoSeleccionado; }
+            set
+            {
+                productoSeleccionado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AgregarProductoCommand { get; }
+        public ICommand EditarProductoCommand { get; }
+        public ICommand EliminarProductoCommand { get; }
+
+        public ProductosViewModel()
+        {
+            productoService = new ProductoService();
+            Productos = new ObservableCollection<Producto>(productoService.ObtenerProductos());
+
+            AgregarProductoCommand = new RelayCommand(AgregarProducto);
+            EditarProductoCommand = new RelayCommand(EditarProducto, PuedeEditarOEliminar);
+            EliminarProductoCommand = new RelayCommand(EliminarProducto, PuedeEditarOEliminar);
+        }
+
+        private void AgregarProducto(object parameter)
+        {
+            var nuevoProducto = new Producto
+            {
+                Nombre = "Nuevo Producto",
+                Precio = 0.0m,
+                Categoria = "Categoría",
+                Stock = 0
+            };
+            productoService.AgregarProducto(nuevoProducto);
+            Productos.Add(nuevoProducto);
+        }
+
+        private void EditarProducto(object parameter)
+        {
+            if (ProductoSeleccionado != null)
+            {
+                productoService.ActualizarProducto(ProductoSeleccionado);
+                // Actualizar la lista de productos si es necesario
+            }
+        }
+
+        private void EliminarProducto(object parameter)
+        {
+            if (ProductoSeleccionado != null)
+            {
+                productoService.EliminarProducto(ProductoSeleccionado.IdProducto);
+                Productos.Remove(ProductoSeleccionado);
+            }
+        }
+
+        private bool PuedeEditarOEliminar(object parameter)
+        {
+            return ProductoSeleccionado != null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
